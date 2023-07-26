526f406e07feb556ee393bcb1185275b0e3b2a88
Kirell Benzi <kirell.benzi@epfl.ch>
2023-07-26 15:51:13 +0200

    various tests

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import CABLES from '../public/js/patch.js';
+import '../public/js/libs.core.min.js';
+import '../public/js/cables.min.js';
+import '../public/js/ops.js';
 
 // disable rubberband effect on mobile devices
-document.getElementById('glcanvas').addEventListener('touchmove', (e) => {
-    e.preventDefault();
-}, false);
+document.getElementById('glcanvas').addEventListener('touchmove', (e)=>{ e.preventDefault(); }, false);
 
 function patchInitialized(patch) {
     // You can now access the patch object (patch), register variable watchers and so on
@@ -13,18 +13,16 @@ function patchFinishedLoading(patch) {
     // The patch is ready now, all assets have been loaded
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.addEventListener('CABLES.jsLoaded', function (event) {
-        CABLES.patch = new CABLES.Patch({
-            patch: CABLES.exportedPatch,
-            "prefixAssetPath": "",
-            "assetPath": "assets/",
-            "jsPath": "js/",
-            "glCanvasId": "glcanvas",
-            "glCanvasResizeToWindow": true,
-            "onPatchLoaded": patchInitialized,
-            "onFinishedLoading": patchFinishedLoading,
-            "canvas": {"alpha": true, "premultipliedAlpha": true} // make canvas transparent
-        });
+document.addEventListener('CABLES.jsLoaded', function (event) {
+    CABLES.patch = new CABLES.Patch({
+        patchFile: 'js/Webflow_intergration.json',
+        "prefixAssetPath": "",
+        "assetPath": "assets/",
+        "jsPath": "js/",
+        "glCanvasId": "glcanvas",
+        "glCanvasResizeToWindow": true,
+        "onPatchLoaded": patchInitialized,
+        "onFinishedLoading": patchFinishedLoading,
+        "canvas":{"alpha":true,"premultipliedAlpha":true} // make canvas transparent
     });
 });
